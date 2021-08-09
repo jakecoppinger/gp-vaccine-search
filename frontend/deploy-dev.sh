447a32f1554e777318967856013c16bc71221bd3
Jake Coppinger <jake@jakecoppinger.com>
2021-08-09 18:57:44 +1000

    Improve frontend deploy command line logging

--- frontend/deploy-dev.sh
+++ frontend/deploy-dev.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+echo "Running build..."
+yarn build
 
+echo "Uploading..."
 aws s3 sync dist/ s3://beta.gpvaccinesearch.com/ --delete
 
 # Don't cache on frontend
 aws s3 cp s3://beta.gpvaccinesearch.com/index.html s3://beta.gpvaccinesearch.com/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
+
+echo "Done."
